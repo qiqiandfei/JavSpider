2e75cbd31b9fa7ceaf0cefb17c08c20bf4127592
樱花飞舞 <31001002+qiqiandfei@users.noreply.github.com>
2020-07-12 00:12:26 +0800

    创建结果文件夹

--- JavSpider/pipelines.py
+++ JavSpider/pipelines.py
@@ -6,20 +6,27 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 import json
 import codecs
+import os
 from JavSpider.settings import USER_CONFIG
 
 class JavspiderPipeline(object):
     def __init__(self):
         condition = USER_CONFIG['condition'][0]
         crawlrule = USER_CONFIG['crawlrule']
+        
         if len(USER_CONFIG['condition']) > 1:
             info = condition + '..._' + crawlrule + '_info.json'
             magnet = condition + '..._' + crawlrule + '_magnet.txt'
         else:
             info = condition + '_' + crawlrule + '_info.json'
             magnet = condition + '_' + crawlrule + '_magnet.txt'
-        self.file = codecs.open(info, 'w', encoding='utf-8')
-        self.txt = codecs.open(magnet, 'w', encoding='utf-8')
+        
+        #创建结果文件夹
+        if not os.path.exists('CrawlResult'):
+            os.mkdir('CrawlResult')
+            
+        self.file = codecs.open('CrawlResult/' + info, 'w', encoding='utf-8')
+        self.txt = codecs.open('CrawlResult/' + magnet, 'w', encoding='utf-8')
         self.splitline = 0
 
     def process_item(self, item, spider):
@@ -27,14 +34,6 @@ class JavspiderPipeline(object):
         self.file.write(line)
         dic = json.loads(line)
         self.txt.write(dic['magnet'] + '\r\n')
-        # if self.splitline == 15:
-        #     self.txt.write('\r\n')
-        #     self.txt.write(dic['magnet'] + '\r\n')
-        #     self.splitline = 0
-        # else:
-        #     dic = json.loads(line)
-        #     self.txt.write(dic['magnet'] + '\r\n')
-        # self.splitline += 1
         return item
 
     def spider_closed(self, spider):
