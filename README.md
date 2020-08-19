953d6433cf466192cf7a379e6c21ff138f5d3cf3
飞 于 <164605545@qq.com>
2020-08-19 08:37:54 +0800

    2020-08-18

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # JavSpider
 根据老师或车牌去javbus抓取磁力地址的爬虫脚本～
 
-# 开发环境
-脚本使用Python开发，需要安装Python3并引入scrapy库及相关依赖
+# 运行环境
 
-引入scrapy库，在命令行下执行：pip install scrapy
+运行原生Python需要安装Python3并引入scrapy库及相关依赖；
+
+没有Python环境的(windows)请直接运行dist文件夹下的‘crawl.exe’
+
 
 # 配置说明
 
@@ -16,7 +18,7 @@
 
 欧美影片抓取填写地址'https://javbus.one'
 
-默认配置：'domain': ['https://www.javbus.com']
+默认配置：domain = https://www.javbus.com
 
 
 2、过滤条件：
@@ -25,7 +27,7 @@
 
 例如：输入'IPZ' 会自动抓取IPZ系列所有磁力（模糊番号抓取耗时较长），建议番号用模糊查询，老师用精准查询，利于分类
 
-默认配置：'condition': ['三上悠亜']
+默认配置：condition = 三上悠亜
 
 
 3、抓取优先级：
@@ -34,7 +36,7 @@
 
 ‘字幕’：自动抓取带中文字幕的，没有字幕抓清晰度高的
 
-默认配置：'crawlrule': '字幕'
+默认配置：crawlrule = 字幕
 
 
 4、是否抓取全部磁力：
@@ -43,7 +45,7 @@
 
 'no': 只抓取审核通过的磁力
 
-默认配置：'crawlall': 'yes'
+默认配置：crawlall = yes
 
 
 5、骑兵 or 步兵 or 全部 有些女优没有下码影片，请确认后再抓取～ 欧美影片请忽略此设置～
@@ -54,24 +56,24 @@
     
 'all': 全部都要！
     
-默认配置：'mosaic': 'all'
+默认配置：mosaic = all
 
 
-# 运行Python原生脚本
-1、打开脚本目录，进入JavSpider文件夹，找到settings.py。找到‘USER_CONFIG’（17行），按需修改参数。
+# 运行Python原生脚本(需要Python环境及依赖库)
+1、打开脚本目录（第一层JavSpider），找到配置文件‘config.ini’,根据需修改参数。
 
 2、开始->运行 输入‘cmd’进入命令行
 
-3、cd到项目目录；或者直接在项目目录下‘Shift+鼠标右键’，选择在此处打开命令行或powershell
+3、cd到脚本目录（第一层JavSpider）；或者直接在脚本目录下‘Shift+鼠标右键’，选择在此处打开命令行或powershell
 
 4、输入命令 scrapy crawl jav  等待程序自动抓取结束(结束标识：INFO: Spider closed (finished))
 
 5、脚本执行结束后，结果目录（CrawlResult）中会生成两个文件 xxx_xxx_info.json和xxx_xxx__magnet.txt；xxx_xxx_info.json保存信息较全，可以作为后续数据处理的数据源；xxx_xxx_magnet.txt为纯磁力，可以直接复制到115离线
 
-# Windows下运行exe
-1、打开脚本目录，进入JavSpider文件夹，找到settings.py。找到‘USER_CONFIG’（17行），按需修改参数。
+# Windows下运行exe（无需Python环境）
+1、打开脚本目录，进入dist文件夹，找到配置文件‘config.ini’，按需修改参数。
 
-2、进入 dist目录，双击‘crawl.exe’执行抓取
+2、双击‘crawl.exe’执行抓取
 
 3、等待执行结束后黑窗自动关闭退出，结果文件保存在 dist/CrawlResult文件夹下
 
--- crawl.py
+++ crawl.py
@@ -7,13 +7,12 @@
  * Porpuse: 
  * Copyright © 2020年 Fei. All rights reserved.
 """
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
 import scrapy.spiderloader
 import scrapy.statscollectors
 import scrapy.logformatter
 import scrapy.dupefilters
 import scrapy.squeues
+
 import scrapy.extensions.spiderstate
 import scrapy.extensions.corestats
 import scrapy.extensions.telnet
@@ -26,11 +25,13 @@ import scrapy.extensions.debug
 import scrapy.extensions.httpcache
 import scrapy.extensions.statsmailer
 import scrapy.extensions.throttle
+
 import scrapy.core.scheduler
 import scrapy.core.engine
 import scrapy.core.scraper
 import scrapy.core.spidermw
 import scrapy.core.downloader
+
 import scrapy.downloadermiddlewares.stats
 import scrapy.downloadermiddlewares.httpcache
 import scrapy.downloadermiddlewares.cookies
@@ -46,28 +47,23 @@ import scrapy.downloadermiddlewares.httpcompression
 import scrapy.downloadermiddlewares.redirect
 import scrapy.downloadermiddlewares.retry
 import scrapy.downloadermiddlewares.robotstxt
+
 import scrapy.spidermiddlewares.depth
 import scrapy.spidermiddlewares.httperror
 import scrapy.spidermiddlewares.offsite
 import scrapy.spidermiddlewares.referer
 import scrapy.spidermiddlewares.urllength
+
 import scrapy.pipelines
+
 import scrapy.core.downloader.handlers.http
 import scrapy.core.downloader.contextfactory
 
 import readini
-import JavSpider
-import JavSpider.spiders
-import JavSpider.settings
-from JavSpider.spiders.jav import JavSpider
-import os
-import sys
-
-# print(os.path.dirname(os.path.realpath(sys.argv[0])))
-cfg = os.path.join(os.path.split(sys.path[0])[0], "scrapy.cfg")
-print(cfg)
-input()
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
 
 process = CrawlerProcess(get_project_settings())
-process.crawl(JavSpider.name)
+process.crawl('jav')
 process.start()
+
